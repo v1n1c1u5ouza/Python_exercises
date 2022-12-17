6ef52b9ac4ee376823a7a32406af46e85b43fcf6
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-17 17:15:24 -0300

    Mad lib game using input and varibles

---
+++ mad_libs_game.py
@@ -0,0 +1,18 @@
+verb_1 = input("Enter a verb of choice, and press enter:")
+adj_1 = input("Enter an adjetive of choice, and press enter:")
+verb_2 = input("Enter the second verb of choice, and press enter:")
+body_part_1 = input("Enter a body part name of choice, and press enter:")
+adverb = input("Enter a adverb of choice, and press enter:")
+body_part_2 = input("Enter a body part name of choice, and press enter:")
+noun_1 = input("Enter a noun of choice, and press enter:")
+verb_3 = input("Enter the third verb of choice, and press enter:")
+animal = input("Enter name of any animal of choice, and press enter:")
+noun_2 = input("Enter a noun of choice, and press enter:")
+verb_4 = input("Enter the fourth verb od choice, and press enter:")
+adj_2 = input("Enter an adjetive of choice, and press enter")
+color = input("Enter any color name of choice, and press enter:")
+
+story = "Most doctors agree that the bicycle of" + verb_1 + " is a/an " + adj_1 + " form of exercise." + verb_2  + " a bicycle enables you to develop your" + body_part_1 +" muscle as well as " + body_part_2 + " beat. More " + noun_1 + " around the world " + verb_3 + " bicycles then drive " + animal + " . no matter what kind of " + noun_2 + " you " + verb_4 + " , always be sure to wear a/an " + adj_2 + " helmet. Make sure to have " + color + " reflectors too! "
+
+
+print(story)
